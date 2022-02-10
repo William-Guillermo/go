df1af11e593dff432837375da652ce2aef04ecb5
William-Guillermo <guillermodelapazw@gmail.com>
2022-02-10 13:57:12 -0600

    variables en Go

---
+++ variables.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	edad := 37 // declaracion corta de variables con el operador :=   va dentro del cuerpo de la funcion {}
+	nombre := "William Guillermo"
+	fmt.Println(nombre, edad, "Vallarta")
+}
