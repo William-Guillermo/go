9217052c81314b2bc4ec802df915341268974c4b
William-Guillermo <guillermodelapazw@gmail.com>
2022-02-11 13:21:36 -0600

    Alcance de variables

--- variables.go
+++ variables.go
@@ -1,9 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+var z int = 38 //como buena practica scoup  dentro del bloque de codigo y no general
 
 func main() {
 	edad := 37 // declaracion corta de variables con el operador :=   va dentro del cuerpo de la funcion {}
 	nombre := "William Guillermo"
 	fmt.Println(nombre, edad, "Vallarta")
+
+	y := 2 // asignacion corta  pero una vez asignada en el programa se debe utilizar
+	x := 3
+	fmt.Println(x + y)
+	fmt.Println(z)
 }
